770c5863c2f4d84c641366adec5b5c58baf5ee60
antonio29398 <antonionapolitano58@yahoo.it>
2025-05-12 11:47:20 +0200

    push scheletro del progetto

---
+++ sase/src/main/java/com/sad_security/sase/SaseApplication.java
@@ -0,0 +1,13 @@
+package com.sad_security.sase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaseApplication.class, args);
+	}
+
+}
---
+++ sase/src/test/java/com/sad_security/sase/SaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sad_security.sase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
